7b6acf2ca59d7029c4c4a6ea9b1fa665a696f528
Darsh374 <dp594@njit.edu>
2020-02-04 19:27:02 -0500

    add: added mulitplication, addition, subtraction, division

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -2,3 +2,15 @@ class Calculator:
 
     def __init__(self):
         pass
+
+    def addition(self, a, b):
+        return a + b
+
+    def subtraction(self, a, b):
+        return a - b
+
+    def multiply(self, a ,b):
+        return a * b
+
+    def divide(self, a, b):
+        return a // b
--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -8,6 +8,25 @@ class MyTestCase(unittest.TestCase):
         calculator = Calculator
         self.assertIsInstance(Calculator, calculator)
 
+    def test_calculator_addition(self, a, b):
+        calculator = Calculator
+        result = calculator.addition(1, 2)
+        self.assertEqual(3, result)
+
+    def test_calculator_subtraction(self, a, b):
+        calculator = Calculator
+        result = calculator.subtraction(1, 2)
+        self.assertEqual(-1, result)
+
+    def test_calculator_multiply(self, a, b):
+        calculator = Calculator
+        result = calculator.multiply(1, 2)
+        self.assertEqual(2, result)
+
+    def test_calculator_divide(self, a, b):
+        calculator = Calculator
+        result = calculator.subtraction(6, 2)
+        self.assertEqual(3, result)
 
 if __name__ == '__main__':
     unittest.main()
